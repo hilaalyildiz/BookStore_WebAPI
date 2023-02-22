0fa68f11415ca19807d42d9691ec6759464710bc
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2023-02-22 18:36:39 +0300

    Proje dosyası ekle.

---
+++ BookStore_WebAPI/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BookStore_WebAPI
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int GenreID { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishDate { get; set; }
+
+    }
+}
---
+++ BookStore_WebAPI/Controllers/BookController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace BookStore_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book{
+                    Id =1,
+                    Title="Lean Startup",
+                    GenreID=1,
+                    PageCount=200,
+                    PublishDate=new DateTime(2001,06,26)
+                },
+            new Book{
+                    Id =2,
+                    Title="Hearland",
+                    GenreID=2,
+                    PageCount=250,
+                    PublishDate=new DateTime(2011,06,16)
+                },
+            new Book{
+                    Id =3,
+                    Title="Dune",
+                    GenreID=3,
+                    PageCount=450,
+                    PublishDate=new DateTime(2014,12,16)
+                }
+
+        };
+
+        [HttpGet]
+        public List<Book> GetBook()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+    }
+}
